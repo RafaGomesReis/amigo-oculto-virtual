b043d6fd78462c4498750022b9f255dcf5dd2c6c
Rafael <raf.gomes.reis@gmail.com>
2025-03-20 20:24:58 -0300

    ultimos-ajustes-no-addevent

--- frontend/src/components/admin/events/EventAdd.tsx
+++ frontend/src/components/admin/events/EventAdd.tsx
@@ -16,7 +16,8 @@ export const EventAdd = ({ refreshAction }: Props) => {
     const [titleField, setTitleField] = useState('');
     const [descriptionField, setDescriptionField] = useState('');
     const [groupedField, setGroupedField] = useState(false);
-    const [errors, setError] = useState<ErrorItem[]>([]);
+    const [errors, setErrors] = useState<ErrorItem[]>([]);
+    const [loading, setLoading] = useState(false);
 
     const eventSchema = z.object({
         titleField: z.string().min(1, 'Título é obrigatório'),
@@ -25,16 +26,19 @@ export const EventAdd = ({ refreshAction }: Props) => {
     });
 
     const handleAddButton = async () => { 
+        setErrors([])
         const data = eventSchema.safeParse({ titleField, descriptionField, groupedField });
 
-        if (!data.success) return setError(getErrorFromZod(data.error));
-
+        if (!data.success) return setErrors(getErrorFromZod(data.error));
+        setLoading(true);
         const eventItem = await api.addEvent({
             title: data.data.titleField,
             description: data.data.descriptionField,
             grouped: data.data.groupedField
         });
+        setLoading(false);
         if (eventItem) refreshAction();
+    
     }
 
     return (
@@ -46,6 +50,7 @@ export const EventAdd = ({ refreshAction }: Props) => {
                     onChange={(e) => setTitleField(e.target.value)}
                     placeholder="digite o título do evento"
                     errorMessage={errors.find(item => item.field === 'titleField')?.message}
+                    disabled={loading}
                 />
             </div>
         
@@ -56,7 +61,7 @@ export const EventAdd = ({ refreshAction }: Props) => {
                     onChange={(e) => setDescriptionField(e.target.value)}
                     placeholder="digite a descrição do evento"
                     errorMessage={errors.find(item => item.field === 'descriptionField')?.message}
-
+                    disabled={loading}
                 />
             </div>
             <div className="mb-5">
@@ -66,11 +71,13 @@ export const EventAdd = ({ refreshAction }: Props) => {
                     checked={groupedField}
                     onChange={(e) => setGroupedField(e.target.checked)}
                     className="block w-5 h-5 mt-3"
+                    disabled={loading}
                 />
             </div>
                 <Button
-                    value="Adicionar"
+                    value={loading ? 'Adicionando...' : 'Adicionar'}
                     onClick={handleAddButton}
+                    disabled={loading}
                 />
         </div>
         
